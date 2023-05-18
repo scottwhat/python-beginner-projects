4ae5e2079d92db6704ba72e53722dbbf82a53126
scottwhat <66903412+scottwhat@users.noreply.github.com>
2023-05-18 19:59:53 +1000

    small projects

---
+++ collections/dictionaries.py
@@ -0,0 +1,13 @@
+
+
+capitals = {"USA" : "Washington D.C",
+            "India" : "New Delhi",
+            "China" : "Beijing",
+            "Russia": "Moscow"
+            }
+
+
+capitems = capitals.items()
+
+for key, value  in capitals.items():
+    print(f"{key}: {value}")
