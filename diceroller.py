1ba17446d17e70e781eb111d81e10fb4822a1c70
scottwhat <66903412+scottwhat@users.noreply.github.com>
2023-05-19 10:18:14 +1000

    cypher

---
+++ diceroller.py
@@ -0,0 +1,49 @@
+import random
+
+
+#ascii art https://www.youtube.com/watch?v=x-Ag2_bJ40Y&list=PLZPZq0r_RZOOkUQbat8LyQii36cJf2SWT&index=29
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘")
+}
+
+dice = []
+total = 0
+num_of_dice = int(input("How many dice?: "))
+
+for die in range(num_of_dice):
+    dice.append(random.randint(1,6))
+
+for die in dice:
+    total += die
+
+print(dice)
+print(total)
---
+++ encryption/cypher-incription.py
@@ -0,0 +1,43 @@
+import random
+import string
+#https://www.youtube.com/watch?v=vsLBErLWBhA&list=PLZPZq0r_RZOOkUQbat8LyQii36cJf2SWT&index=30
+#import char constants from the string module
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+#typecast convert the string to a list
+chars = list(chars)
+key = chars.copy()
+
+random.shuffle(key)
+
+#!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+print(f"chars: {chars}")
+print(f"key: {key}")
+
+
+#encrypt
+
+plain_text = input("Enter a message to encrypt")
+
+cipher_text = ""
+
+decypher_text = ""
+
+
+
+#ENCYPHER
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print("Encrypted message:  " + cipher_text)
+
+#DECYPHER
+
+for letter in cipher_text:
+    index = key.index(letter)
+    decypher_text += chars[index]
+
+print("Decrypted message:  " + decypher_text)
+
+
+
